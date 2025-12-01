fd6dc4614787a8a02380794e2cd38135eb695d6e
Arslan Malik <arsaaal786@gmail.com>
2025-12-01 16:08:42 +0500

    updated

--- app/src/main/java/com/example/antitheftapp/ui/HomeScreen.kt
+++ app/src/main/java/com/example/antitheftapp/ui/HomeScreen.kt
@@ -9,9 +9,11 @@ import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.antitheftapp.R
@@ -21,6 +23,7 @@ import java.util.Objects
 class HomeScreen : AppCompatActivity() {
 
     private lateinit var btnArm : Button
+    private lateinit var tvInstruction : TextView
     private var isArmed = false
 
 
@@ -35,17 +38,26 @@ class HomeScreen : AppCompatActivity() {
         }
 
         btnArm = findViewById(R.id.btnArm)
+        tvInstruction = findViewById<TextView>(R.id.tvInstruction)
 
 
         btnArm.setOnClickListener {
             if (isArmed){
                 isArmed = false
-                btnArm.text = "START"
+                btnArm.text = "ACTIVE"
+                tvInstruction.text = "Tap to activate protection"
+                tvInstruction.setTextColor(ContextCompat.getColor(this,R.color.lightGrey))
+                btnArm.background =ContextCompat.getDrawable(this,R.drawable.circular_btn_unselected)
+                btnArm.setTextColor(ContextCompat.getColor(this,R.color.white))
               stopService(Intent(this, AntiTheftService::class.java))
             }else {
                 isArmed = true
-                btnArm.text = "STOP"
-              startForegroundService(Intent(this, AntiTheftService::class.java))
+                btnArm.text = "ACTIVATED"
+                tvInstruction.text = "Protection is active"
+                tvInstruction.setTextColor(ContextCompat.getColor(this,R.color.green))
+                btnArm.background = ContextCompat.getDrawable(this,R.drawable.circular_button)
+                btnArm.setTextColor(ContextCompat.getColor(this,R.color.white))
+                startForegroundService(Intent(this, AntiTheftService::class.java))
             }
         }
     }
